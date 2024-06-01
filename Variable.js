188e1371d86b1732ba4561f975f1f547938055c6
syedafroze <safroze112@gmail.com>
2024-06-01 16:09:53 +0530

    updated

---
+++ Variable.js
@@ -0,0 +1,56 @@
+//variable 
+
+//var let const 
+
+
+//global scope   var 
+
+//block scope   let and const 
+
+// {} ->braces    ()-> paranthesis
+
+{
+    //code   ///block
+}
+
+
+{
+let age1 =undefined;
+age1= 29;
+
+const age =30;
+
+let myName = 'syed';
+ // age=age+1;
+
+const city = 'Hyd';
+console.log(age);//28
+}
+
+//           0
+const arr = [2,5,6]; // reference vlue 
+
+
+console.log(arr);
+
+
+//Datatypes
+
+/*
+PrimitveDatatype
+
+Number 
+String
+Boolean
+Undefined
+Null
+
+
+Symbol
+BigInt 
+
+Non primitive Datatype ( Reference Datatype)
+Object
+Function
+
+*/
